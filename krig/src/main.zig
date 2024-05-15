feae4bbfbe0e8e8eca06abc39dfb349798f74875
Kristoffer Grönlund <k@ziran.se>
2024-05-15 13:51:06 +0200

    strip out default stuff

--- krig/src/main.zig
+++ krig/src/main.zig
@@ -1,143 +1,71 @@
 const std = @import("std");
+const fmt = std.fmt;
 const cart = @import("cart-api");
 
 export fn start() void {}
 
-var scene: enum { intro, game } = .intro;
+const black = defColor(0x000000);
+const white = defColor(0xffffff);
+const zig = defColor(0xF7A41D);
+const red = defColor(0xFF0000);
+const green = defColor(0x00FF00);
+const blue = defColor(0x0000FF);
+const ziggy1 = defColor(0xf98a31);
+const ziggy3 = defColor(0xe56c2c);
+const ziggy4 = defColor(0xf15a29);
+const ziggy5 = defColor(0xc54827);
+const ziggy6 = defColor(0xfbb040);
+const ziggy7 = defColor(0xf09d2e);
+const ziggy8 = defColor(0xeaebec);
+const ziggy2 = defColor(0x231f20);
+const ziggy9 = defColor(0x212121);
 
-const black: cart.DisplayColor = .{ .r = 0, .g = 0, .b = 0 };
-const white: cart.DisplayColor = .{ .r = 31, .g = 63, .b = 31 };
-const zig: cart.DisplayColor = rgbColor(0xF7A41D);
-const red: cart.DisplayColor = rgbColor(0xFF0000);
-const green: cart.DisplayColor = rgbColor(0x00FF00);
-const blue: cart.DisplayColor = rgbColor(0x0000FF);
 
-inline fn rgbColor(rgb: u24) cart.DisplayColor {
+inline fn defColor(rgb: u24) cart.NeopixelColor {
     return .{
-        .r = @intCast(((rgb >> 16) & 0xff) / 8),
-        .g = @intCast(((rgb >> 8) & 0xff) / 4),
-        .b = @intCast((rgb & 0xff) / 8),
+        .r = @intCast((rgb >> 16) & 0xff),
+        .g = @intCast((rgb >> 8) & 0xff),
+        .b = @intCast(rgb & 0xff),
     };
 }
 
-export fn update() void {
-    switch (scene) {
-        .intro => scene_intro(),
-        .game => scene_game(),
-    }
+inline fn rgb565(clr: cart.NeopixelColor) cart.DisplayColor {
+    return .{
+        .r = @intCast(clr.r / 8),
+        .g = @intCast(clr.g / 4),
+        .b = @intCast(clr.b / 8),
+    };
 }
 
 var ticks: u8 = 0;
 
-fn scene_intro() void {
+export fn update() void {
     set_background();
-
-    @memset(cart.neopixels, .{
-        .r = 0,
-        .g = 0,
-        .b = 0,
-    });
-
-    if (ticks / 128 == 0) {
-        // Make the neopixel 24-bit color LEDs a nice Zig orange
-        @memset(cart.neopixels, .{
-            .r = 247,
-            .g = 164,
-            .b = 29,
-        });
-    }
 
     cart.rect(.{
         .x = @intCast(cart.screen_width / 2 - 50),
         .y = @intCast(cart.screen_height / 2 - 25),
         .width = 100,
         .height = 50,
-        .stroke_color = black,
-        .fill_color = zig,
+        .stroke_color = rgb565(ziggy8),
+        .fill_color = rgb565(zig),
     });
 
-    if (ticks == 0) cart.red_led.* = !cart.red_led.*;
-    if (cart.controls.start) scene = .game;
+    const slowTick: u8 = @intCast(ticks / 4);
 
-    ticks +%= 4;
-}
+    var buf: [32]u8 = undefined;
+    _ = fmt.bufPrint(&buf, "{d}\n", .{slowTick}) catch "err";
 
-const Player = enum(u8) { x = 0, o = 1, none = std.math.maxInt(u8) };
-
-var selected_x: u8 = 0;
-var selected_y: u8 = 0;
-var control_cooldown: bool = false;
-var turn: Player = .x;
-var state: [3][3]Player = @bitCast([1]Player{.none} ** 9);
-
-fn scene_game() void {
-    set_background();
-
-    const title = "TIC-TAC-TOE";
     cart.text(.{
-        .text_color = .{ .r = 31, .g = 63, .b = 31 },
-        .str = title,
-        .x = @intCast((cart.screen_width - cart.font_width * title.len) / 2),
-        .y = 10,
+        .str = &buf,
+        .x = 4,
+        .y = 4,
+        .text_color = rgb565(white),
     });
 
-    const instructions = "D-PAD + SELECT";
-    cart.text(.{
-        .text_color = .{ .r = 31, .g = 63, .b = 31 },
-        .str = instructions,
-        .x = @intCast((cart.screen_width - cart.font_width * instructions.len) / 2),
-        .y = cart.screen_height - cart.font_height - 10,
-    });
+    cart.red_led.* = !cart.red_led.*;
 
-    for (0..3) |y| {
-        for (0..3) |x| {
-            cart.rect(.{
-                .stroke_color = .{ .r = 31, .g = 63, .b = 31 },
-                .fill_color = if (x == selected_x and y == selected_y) .{ .r = 31 / 2, .g = 63 / 2, .b = 31 / 2 } else null,
-                .x = @intCast(cart.screen_width / 2 + x * 19 - 10 * 3),
-                .y = @intCast(cart.screen_height / 2 + y * 19 - 10 * 3),
-                .width = 20,
-                .height = 20,
-            });
-
-            cart.text(.{
-                .text_color = .{ .r = 31, .g = 63, .b = 31 },
-                .str = switch (state[y][x]) {
-                    .x => "X",
-                    .o => "O",
-                    .none => "",
-                },
-                .x = @intCast(cart.screen_width / 2 + x * 19 - 10 * 3 + 7),
-                .y = @intCast(cart.screen_height / 2 + y * 19 - 10 * 3 + 7),
-            });
-        }
-    }
-
-    if (!control_cooldown) {
-        if (cart.controls.left) selected_x -|= 1;
-        if (cart.controls.right and selected_x != 2) selected_x += 1;
-        if (cart.controls.up) selected_y -|= 1;
-        if (cart.controls.down and selected_y != 2) selected_y += 1;
-        if (cart.controls.select and state[selected_y][selected_x] == .none) {
-            state[selected_y][selected_x] = turn;
-            turn = switch (turn) {
-                .x => .o,
-                .o => .x,
-                else => unreachable,
-            };
-
-            if (check_win()) {
-                turn = .x;
-                @memset(@as(*[9]Player, @ptrCast(&state)), .none);
-                scene = .intro;
-                selected_x = 0;
-                selected_y = 0;
-            }
-        }
-    }
-
-    control_cooldown = false;
-    if (cart.controls.left or cart.controls.right or cart.controls.up or cart.controls.down or cart.controls.select) control_cooldown = true;
+    ticks +%= 1;
 }
 
 fn set_background() void {
@@ -148,13 +76,4 @@ fn set_background() void {
         .g = @intFromFloat(ratio * 63),
         .b = @intFromFloat(ratio * 31),
     });
-}
-
-fn check_win() bool {
-    for (0..3) |i| {
-        if (state[i][0] != .none and state[i][0] == state[i][1] and state[i][1] == state[i][2]) return true;
-        if (state[0][i] != .none and state[0][i] == state[1][i] and state[1][i] == state[2][i]) return true;
-    }
-
-    return (state[0][0] != .none and state[0][0] == state[1][1] and state[1][1] == state[2][2]) or (state[0][2] != .none and state[0][2] == state[1][1] and state[1][1] == state[2][0]);
 }
