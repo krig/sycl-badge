bdf58ccd6dffdc1b794de9e74eaba368a57a1409
Kristoffer Grönlund <k@ziran.se>
2024-05-15 20:19:25 +0200

    basic space game

--- krig/src/main.zig
+++ krig/src/main.zig
@@ -15,10 +15,14 @@ const ziggy4 = defColor(0xf15a29);
 const ziggy5 = defColor(0xc54827);
 const ziggy6 = defColor(0xfbb040);
 const ziggy7 = defColor(0xf09d2e);
-const ziggy8 = defColor(0xeaebec);
 const ziggy2 = defColor(0x231f20);
-const ziggy9 = defColor(0x212121);
+const ziggy8 = defColor(0xeaebec);
+const light = defColor(0x555555);
+const dark = defColor(0x222222);
 
+const shipTop = defColor(0xF7A41D);
+// const shipBottom = defColor(0xf58f0a);
+const shipBottom = defColor(0xc37b07);
 
 inline fn defColor(rgb: u24) cart.NeopixelColor {
     return .{
@@ -44,45 +48,230 @@ inline fn rgb565(clr: cart.NeopixelColor) cart.DisplayColor {
     };
 }
 
-const numLines = 8;
-var lines: [numLines]f32 = undefined;
-var lineColor: [numLines]cart.NeopixelColor = undefined;
+const Star = struct {
+    x: f32,
+    y: f32,
+    speed: f32,
+    color: cart.DisplayColor,
+};
+
+var rand_seed: u16 = 5381;
+fn rand_float() f32 {
+    rand_seed = std.math.rotl(u16, rand_seed, 5) +% rand_seed;
+    return @as(f32, @floatFromInt(rand_seed)) / @as(f32, std.math.maxInt(u16)); 
+}
+
+const NumStars = 32;
+var starfield: [NumStars]Star = undefined;
+
+const Player = struct {
+    x: f32,
+    y: f32,
+    speed: f32,
+    health: u8,
+    cooldown: u8,
+};
+var player: Player = undefined;
+const MaxHealth: u8 = 5;
+
+const BulletType = enum {
+    dot,
+    cross,
+    ball,
+};
+
+const Bullet = struct {
+    x: f32,
+    y: f32,
+    dx: f32,
+    dy: f32,
+    typ: BulletType,
+    live: bool,
+};
+const MaxBullets = 64;
+var bullets: [MaxBullets]Bullet = undefined;
 
 export fn start() void {
-    for (lines, 0..) |_, i| {
-        const v = @as(f32, @floatFromInt(i)) / numLines;
-        lines[i] = v;
-        lineColor[i] = blend(black, zig, 0.2 + v*0.8);
+    for (&starfield) |*star| {
+        const speed = rand_float();
+        star.* = .{
+            .x = rand_float() * cart.screen_width,
+            .y = rand_float() * cart.screen_height,
+            .speed = speed,
+            .color = rgb565(blend(dark, light, speed)),
+        };
     }
+    for (&bullets) |*bullet| bullet.*.live = false;
+    player = .{
+        .x = 8.0,
+        .y = cart.screen_height / 2,
+        .speed = 0.0,
+        .health = MaxHealth,
+        .cooldown = 0,
+    };
+}
+
+fn draw_stars() void {
+    const shaky = (player.y / cart.screen_height) * -15.0;
+    for (&starfield) |*star| {
+        cart.hline(.{
+            .x = @intFromFloat(star.*.x),
+            .y = @intFromFloat(star.*.y + shaky * star.*.speed),
+            .len = @intFromFloat(star.*.speed * 4.0 + 1.0),
+            .color = star.*.color,
+        });
+        var x = star.*.x - star.*.speed * 2.0;
+        if (x < 0.0) x = @floatFromInt(cart.screen_width);
+        star.*.x = x;
+    }
+}
+
+fn spawn_bullet(bullet: Bullet) void {
+    for (&bullets) |*b| {
+        if (b.live) continue;
+        b.* = bullet;
+        break;
+    }
+}
+
+fn draw_bullets() void {
+    // move all live bullets
+    // if player is firing: spawn new bullet
+    if (cart.controls.a) {
+        if (player.cooldown > 0) {
+            player.cooldown -= 1;
+        } else {
+            player.cooldown = 4;
+            spawn_bullet(.{
+                .x = player.x + 7.0 + (rand_float() - 0.5) * 3.0,
+                .y = player.y + (rand_float() - 0.5),
+                .dx = 3.0 + (rand_float() * 0.2),
+                .dy = 0.5 * (rand_float() - 0.5),
+                .typ = .dot,
+                .live = true,
+            });
+        }
+    }
+    for (&bullets) |*bullet| {
+        if (!bullet.live) continue;
+        bullet.*.x += bullet.dx;
+        bullet.*.y += bullet.dy;
+        if (bullet.x > cart.screen_width) bullet.*.live = false;
+        if (bullet.y > cart.screen_height) bullet.*.live = false;
+        if (bullet.x < 0) bullet.*.live = false;
+        if (bullet.y < 0) bullet.*.live = false;
+        if (bullet.live) {
+            if (bullet.typ == .dot) {
+                cart.rect(.{
+                    .x = @intFromFloat(bullet.x),
+                    .y = @intFromFloat(bullet.y),
+                    .width = 1,
+                    .height = 1,
+                    .fill_color = rgb565(white),
+                });
+            } else {
+                cart.hline(.{
+                    .x = @intFromFloat(bullet.x - 1),
+                    .y = @intFromFloat(bullet.y),
+                    .len = 3,
+                    .color = rgb565(white),
+                });
+                cart.vline(.{
+                    .x = @intFromFloat(bullet.x),
+                    .y = @intFromFloat(bullet.y - 1),
+                    .len = 3,
+                    .color = rgb565(white),
+                });
+            }
+        }
+    }
+}
+
+fn draw_player() void {
+    if (cart.controls.up) {
+        player.speed = @max(-3.0, player.speed - 0.2);
+    }
+    if (cart.controls.down) {
+        player.speed = @min(3.0, player.speed + 0.2);
+    }
+    if (!cart.controls.up and !cart.controls.down) {
+        player.speed = player.speed * 0.66;
+    }
+    const speed = player.speed;
+    player.y = player.y + player.speed;
+    if (player.y < 8.0) {
+        player.y = 8.0;
+        player.speed = 0.0;
+    }
+    if (player.y > @as(f32, cart.screen_height) - 8.0) {
+        player.y = @as(f32, cart.screen_height) - 8.0;
+        player.speed = 0.0;
+    }
+    const xpos: i32 = @intFromFloat(player.x - @as(f32, @floatFromInt(player.cooldown)) * 0.5);
+    if (speed < -0.1) {
+        cart.hline(.{
+            .x = xpos,
+            .y = @intFromFloat(player.y),
+            .len = 8.0,
+            .color = rgb565(shipBottom),
+        });
+        cart.hline(.{
+            .x = xpos,
+            .y = @intFromFloat(player.y - 1),
+            .len = 3,
+            .color = rgb565(shipBottom),
+        });
+    } else if (speed > 0.1) {
+        cart.hline(.{
+            .x = xpos,
+            .y = @intFromFloat(player.y),
+            .len = 8.0,
+            .color = rgb565(shipTop),
+        });
+        cart.hline(.{
+            .x = xpos,
+            .y = @intFromFloat(player.y + 1),
+            .len = 3,
+            .color = rgb565(shipTop),
+        });
+    } else {
+        cart.hline(.{
+            .x = xpos,
+            .y = @intFromFloat(player.y),
+            .len = 8.0,
+            .color = rgb565(shipTop),
+        });
+    }
+
+    // draw health with neopixels
+    for (cart.neopixels, 0..) |*np, i| {
+        if (player.health > i) {
+            np.* = green;
+        } else {
+            np.* = red;
+        }
+    }
+}
+
+fn draw_enemies() void {
 }
 
 export fn update() void {
     set_background();
-
-    const half_h: u8 = @intCast(cart.screen_height / 2);
-
-    const speed: f32 = 0.008;
-    for (lines, 0..) |life, i| {
-        var newlife = life + speed;
-        if (newlife >= 1.0) newlife = 0.0;
-        const iclr: u8 = @intFromFloat(newlife * numLines);
-        const ipos: u32 = @intFromFloat((newlife*newlife) * half_h);
-        const drawpos: u8 = @intCast(ipos % @as(u32, half_h));
-        cart.line(.{
-            .x1 = 0,
-            .y1 = @intCast(half_h + drawpos),
-            .x2 = cart.screen_width - 1,
-            .y2 = @intCast(half_h + drawpos),
-            .color = rgb565(lineColor[iclr]),
-        });
-        lines[i] = newlife;
-    }
-
-    cart.red_led.* = !cart.red_led.*;
+    draw_stars();
+    draw_bullets();
+    draw_enemies();
+    draw_player();
+    cart.text(.{
+        .str = "@krig",
+        .x = (cart.screen_width - cart.font_width * 5) / 2,
+        .y = cart.screen_height - 12,
+        .text_color = rgb565(ziggy4),
+    });
 }
 
 fn set_background() void {
-    const ratio = (4095 - @as(f32, @floatFromInt(cart.light_level.*))) / 4095 * 0.2;
+    const ratio = (4095 - @as(f32, @floatFromInt(cart.light_level.*))) / 4095 * 0.1;
 
     @memset(cart.framebuffer, cart.DisplayColor{
         .r = @intFromFloat(ratio * 31),
